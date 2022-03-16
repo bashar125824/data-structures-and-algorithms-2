cda328e93e68c550d039f3c23093c073212b1ac4
bashar125824 <bashar.alrefae.7@gmail.com>
2022-03-16 11:52:36 +0300

    Challenge 02 DONE

--- array-insert-shift/array-insert-shift.cs
+++ array-insert-shift/array-insert-shift.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataStructure.data_structure_console.array_reverse
+namespace DataStructure.data_structure_console.array_insert_shift
 {
-    class array_reverse
+    class array_insert_shift
     {
-
     }
 }
